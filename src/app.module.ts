330c1de4ca961e0517955ad889049c59fccd6ae0
Marcin <belkocik97@interia.pl>
2023-10-15 19:03:26 +0200

    feat: add env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { PrismaModule } from './prisma/prisma.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './common/guards';
 import { ConfigModule } from '@nestjs/config';
+import { validate } from './config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validate,
     }),
     AuthModule,
     PrismaModule,
